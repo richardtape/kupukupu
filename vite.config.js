980d4e64e5862c176a0e4c49afc59e6bda5018f2
Rich Tape <richard.tape@ubc.ca>
2025-01-07 16:57:31 -0800

    Adjusted base route to show the main index.html

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,19 @@ export default {
     strictPort: true,
     open: true
   },
+  plugins: [{
+    name: 'configure-response-headers',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url === '/') {
+          res.writeHead(302, { Location: '/pages/index.html' });
+          res.end();
+        } else {
+          next();
+        }
+      });
+    }
+  }],
   build: {
     outDir: '../dist',
     emptyOutDir: true,
